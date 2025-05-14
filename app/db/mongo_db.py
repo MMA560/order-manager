426c7562b3544ea1df0ae7bfbb38a56d0b46a18c
MMA560 <mo3geza380@gmail.com>
2025-05-14 18:24:07 +0300

    Add try and except

--- app/db/mongo_db.py
+++ app/db/mongo_db.py
@@ -2,6 +2,10 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from motor.motor_asyncio import AsyncIOMotorCollection
 
-client = AsyncIOMotorClient("mongodb+srv://mma320:zHcJYVCcKoI5kRYh@cluster0.njoseeb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = AsyncIOMotorClient(
+    "mongodb+srv://mma320:zHcJYVCcKoI5kRYh@cluster0.njoseeb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    tls=True,
+    tlsAllowInvalidCertificates=True  # في حال كانت الشهادات غير صالحة
+)
 db = client["my_products"]  # اسم قاعدة البيانات التي تريد استخدامها
 products_collection = db["products"]  # الـ Collection لحفظ المنتجات
